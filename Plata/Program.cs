da9ba0a687a2737338272d85422cb782b4aacf6d
Laze <dimovski.laze@gmail.com>
2020-06-19 11:56:51 +0200

    .

--- Plata/Program.cs
+++ Plata/Program.cs
@@ -17,6 +17,8 @@ namespace Plata
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            int a;
         }
     }
 }
